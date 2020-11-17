9a1b425ad567850608392cebb81d56b93562cd84
DESKTOP-9AIRTCB\123 <chetl@mail.ru>
2020-11-17 03:35:15 +0300

    1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        val toolbar = findViewById(R.id.my_toolbar_1) as? Toolbar
+////        toolbar?.background?.alpha=0
+//        setSupportActionBar(toolbar)
+//////
+//        supportActionBar?.apply {
+//            setDisplayShowHomeEnabled(true)
+//        }
     }
 }
