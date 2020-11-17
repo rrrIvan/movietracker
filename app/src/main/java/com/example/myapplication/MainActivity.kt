ae45cc8730884516d0f515a1a145d41150b5d26b
DESKTOP-9AIRTCB\123 <chetl3308@gmail.com>
2020-11-18 01:24:48 +0300

    inset work

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
-
+import android.view.View
+import android.view.View.*
+import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.updateLayoutParams
+import androidx.core.view.updatePadding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val toolbar = findViewById(R.id.my_toolbar_1) as? Toolbar
-////        toolbar?.background?.alpha=0
-//        setSupportActionBar(toolbar)
-//////
-//        supportActionBar?.apply {
-//            setDisplayShowHomeEnabled(true)
-//        }
+
+        viewroot.systemUiVisibility = SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        ViewCompat.setOnApplyWindowInsetsListener(toolbar_trans) { view, insets ->
+            view.updatePadding(top = insets.systemWindowInsetTop)
+            insets
+        }
     }
+
 }
