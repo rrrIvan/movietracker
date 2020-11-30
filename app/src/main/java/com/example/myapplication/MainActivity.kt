30512999c9962bfdb02f321996fc2c16a79893a4
DESKTOP-9AIRTCB\123 <chetl3308@gmail.com>
2020-11-30 05:28:52 +0300

    макет нарисован

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.updateLayoutParams
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar.*
+import kotlinx.android.synthetic.main.fragment_movie_list2.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,13 +24,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_movie_list2)
 
+//
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.setDecorFitsSystemWindows(false)
-        } else {
+        }
+            else {
 
-            viewroot.systemUiVisibility = SYSTEM_UI_FLAG_LAYOUT_STABLE or
+            viewroot1.systemUiVisibility = SYSTEM_UI_FLAG_LAYOUT_STABLE or
                     SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         }
         ViewCompat.setOnApplyWindowInsetsListener(toolbar_trans) { view, insets ->
