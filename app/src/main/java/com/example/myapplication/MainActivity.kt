7b46553aad4261e8c0e768fead0664a7eae31f23
Nikita Balashov <n.balashov@crmsensor.ru>
2020-12-01 10:12:33 +0300

    Refactoring

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,10 +8,8 @@ import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.ViewCompat
 import androidx.core.view.updateLayoutParams
 import kotlinx.android.synthetic.main.activity_main.fragments_container
-import kotlinx.android.synthetic.main.activity_main.root_view
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,20 +27,21 @@ class MainActivity : AppCompatActivity() {
                     SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         }
 
+        val idStatusBarHeight: Int = resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (idStatusBarHeight > 0) {
+            topMargin = resources.getDimensionPixelSize(idStatusBarHeight)
+        }
+
         if (savedInstanceState == null)
-            ViewCompat.setOnApplyWindowInsetsListener(root_view) { view, insets ->
-                topMargin = insets.systemWindowInsetTop
-                supportFragmentManager.beginTransaction()
-                    .apply {
-                        add(
-                            R.id.fragments_container,
-                            FragmentMoviesList.newInstance()
-                        )
-                        addToBackStack(null)
-                        commit()
-                    }
-                insets
-            }
+            supportFragmentManager.beginTransaction()
+                .apply {
+                    add(
+                        R.id.fragments_container,
+                        FragmentMoviesList.newInstance()
+                    )
+                    addToBackStack(null)
+                    commit()
+                }
     }
 
     override fun setSupportActionBar(toolbar: Toolbar?) {
