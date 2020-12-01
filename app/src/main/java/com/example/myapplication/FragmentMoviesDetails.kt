0121ca6c82ed1a0b8d9587ab09cee33c9e4e6daa
Nikita Balashov <n.balashov@crmsensor.ru>
2020-12-01 09:38:06 +0300

    Toolbar margin updates

--- app/src/main/java/com/example/myapplication/FragmentMoviesDetails.kt
+++ app/src/main/java/com/example/myapplication/FragmentMoviesDetails.kt
@@ -1,13 +1,10 @@
 package com.example.myapplication
 
-import android.app.Activity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toolbar
-
+import androidx.fragment.app.Fragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +17,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class FragmentMoviesDetails : Fragment() {
+
     // TODO: Rename and change types of parameters
     private var param1: Int? = null
     private var param2: String? = null
@@ -30,7 +28,6 @@ class FragmentMoviesDetails : Fragment() {
             param1 = it.getInt(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-
     }
 
     override fun onCreateView(
@@ -40,20 +37,15 @@ class FragmentMoviesDetails : Fragment() {
         return inflater.inflate(R.layout.fragment_movies_details, container, false)
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        try {
-            view.findViewById<Toolbar>(R.id.toolbar_trans1).updateMarginTop(param1?:0)
-        }catch (e:Exception){
-            e.printStackTrace()
+        (activity as MainActivity?)?.let {
+            it.setSupportActionBar(view.findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar_trans1))
         }
-
-
-
     }
 
     companion object {
+
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
@@ -64,7 +56,7 @@ class FragmentMoviesDetails : Fragment() {
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: Int, param2: String="") =
+        fun newInstance(param1: Int = 0, param2: String = "") =
             FragmentMoviesDetails().apply {
                 arguments = Bundle().apply {
                     putInt(ARG_PARAM1, param1)
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,5 @@
 package com.example.myapplication
 
-import android.animation.ValueAnimator
-import android.annotation.SuppressLint
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.View
@@ -10,20 +7,21 @@ import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.updateLayoutParams
-import kotlinx.android.synthetic.main.activity_main.*
-
+import kotlinx.android.synthetic.main.activity_main.fragments_container
+import kotlinx.android.synthetic.main.activity_main.root_view
 
 class MainActivity : AppCompatActivity() {
 
+    private var topMargin: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.setDecorFitsSystemWindows(false)
         } else {
 
@@ -33,25 +31,28 @@ class MainActivity : AppCompatActivity() {
 
         if (savedInstanceState == null)
             ViewCompat.setOnApplyWindowInsetsListener(root_view) { view, insets ->
+                topMargin = insets.systemWindowInsetTop
                 supportFragmentManager.beginTransaction()
                     .apply {
                         add(
                             R.id.fragments_container,
-                            FragmentMoviesList.newInstance(insets.systemWindowInsetTop)
+                            FragmentMoviesList.newInstance()
                         )
                         addToBackStack(null)
                         commit()
                     }
                 insets
             }
-
     }
 
+    override fun setSupportActionBar(toolbar: Toolbar?) {
+        super.setSupportActionBar(toolbar)
+        toolbar?.updateMarginTop(topMargin)
+    }
 }
 
 public fun View.updateMarginTop(value: Int) {
     updateLayoutParams<ViewGroup.MarginLayoutParams> {
         topMargin = value
     }
-
 }
