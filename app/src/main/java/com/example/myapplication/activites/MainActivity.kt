5bdf9b6e90574e7366a16bffaa662c5882c31e06
DESKTOP-9AIRTCB\123 <chetl3308@gmail.com>
2020-12-12 22:24:06 +0300

    collapsing to movie details

--- app/src/main/java/com/example/myapplication/activites/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/activites/MainActivity.kt
@@ -6,7 +6,9 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.myapplication.R
+import com.example.myapplication.data.Movie
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.fragments.FragmentMoviesDetails
 import com.example.myapplication.fragments.FragmentMoviesList
 import com.example.myapplication.utils.updateMarginTop
 
@@ -33,19 +35,6 @@ class MainActivity : AppCompatActivity() {
                 }
     }
 
-    // private fun setFullScreen() {
-    //     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-    //         window.setDecorFitsSystemWindows(false)
-    //     } else {
-    //         view.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-    //             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-    //     }
-    //     val idStatusBarHeight: Int =
-    //         resources.getIdentifier("status_bar_height", "dimen", "android")
-    //     if (idStatusBarHeight > 0) {
-    //         topMargin = resources.getDimensionPixelSize(idStatusBarHeight)
-    //     }
-    // }
 
     override fun setSupportActionBar(toolbar: Toolbar?) {
         super.setSupportActionBar(toolbar)
--- app/src/main/java/com/example/myapplication/fragments/FragmentMoviesDetails.kt
+++ app/src/main/java/com/example/myapplication/fragments/FragmentMoviesDetails.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.fragments
 
+import android.annotation.SuppressLint
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,7 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.core.view.marginStart
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -19,20 +22,21 @@ import com.example.myapplication.custom_raiting_bar.RatingBarSvg
 import com.example.myapplication.data.Movie
 import com.example.myapplication.utils.loadImage
 import com.example.myapplication.utils.toPx
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
 class FragmentMoviesDetails : Fragment() {
 
     private lateinit var recycler: RecyclerView
     private lateinit var param1: Movie
-    private lateinit var frBackdrop: ImageView
+    // private lateinit var frBackdrop: ImageView
     private lateinit var frRating: RatingBarSvg
     private lateinit var frIsLike: ImageView
-    private lateinit var frTtle: TextView
+    // private lateinit var frTitle: TextView
     private lateinit var frAgeLimit: TextView
     private lateinit var frTags: TextView
     private lateinit var frReviews: TextView
     private lateinit var frOverview: TextView
-    private lateinit var back: ImageView
+    // private lateinit var back: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,21 +51,31 @@ class FragmentMoviesDetails : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_movies_details, container, false)
-        frBackdrop = view.findViewById(R.id.DetailPoster)
+        // frBackdrop = view.findViewById(R.id.DetailPoster)
         frRating = view.findViewById(R.id.DetailStars)
         frIsLike = view.findViewById(R.id.DetailLike)
-        frTtle = view.findViewById(R.id.DetailTitle)
+        // frTitle = view.findViewById(R.id.DetailTitle)
         frAgeLimit = view.findViewById(R.id.DetailAge)
         frTags = view.findViewById(R.id.DetailTags)
         frReviews = view.findViewById(R.id.DetailReview)
         frOverview = view.findViewById(R.id.DetailOverview)
-        back = view.findViewById(R.id.DetailBackButton)
+        // back = view.findViewById(R.id.DetailBackButton)
         return view
     }
 
+    @SuppressLint("RestrictedApi")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        (activity as MainActivity?)?.setSupportActionBar(view.findViewById(R.id.toolbar_trans1))
+        val toolbar = view.findViewById<Toolbar>(R.id.toolbar)
+
+
+        (activity as MainActivity?)?.apply {
+            setSupportActionBar(toolbar)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayShowHomeEnabled(true)
+        }
+
+
         val lManager = LinearLayoutManager(context)
         lManager.orientation = RecyclerView.HORIZONTAL
         recycler = view.findViewById<RecyclerView>(R.id.rv_actors)
@@ -72,21 +86,18 @@ class FragmentMoviesDetails : Fragment() {
         }
 
         param1.apply {
-            loadImage(view.context, backdrop, frBackdrop)
+            // loadImage(view.context, backdrop, frBackdrop)
             val like = if (is_like) R.drawable.like else R.drawable.no_like
             val drawable: Drawable? = ContextCompat.getDrawable(view.context, like)
             frTags.text = genres.joinToString(separator = ", ")
             frIsLike.setImageDrawable(drawable)
             frReviews.text = votes.toString().plus(" REVIEWS")
             frRating.rating = ratings / 2
-            frTtle.text = title
+            // frTitle.text = title
             frAgeLimit.text = age.toString().plus("+")
             frOverview.text = overview
         }
 
-        back.setOnClickListener {
-            this.fragmentManager?.popBackStack()
-        }
     }
 
     override fun onStart() {
