d65f8539a58942e0af144a2e821e2407f2416096
DESKTOP-9AIRTCB\123 <chetl3308@gmail.com>
2020-12-30 17:25:37 +0300

    test

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
+//123
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
